248d7775eec10ee6046365dc4a739c5c22dfb9ec
Huseyn Alizade <huseyna@maestro.st>
2024-01-22 16:50:17 +0400

    ContainsDuplicate problem

---
+++ ContainsDuplicate.js
@@ -0,0 +1,52 @@
+let numbers = [1, 2, 3, 4,1]
+
+
+// solution 1
+// Time O(n2)
+
+// for (let i = 0; i < numbers.length; i++) {
+//     for (let j = i; j < numbers.length; j++) {
+//         if (numbers[i] == numbers[j]) {
+//             return console.log(true)
+//         }
+//     }
+// }
+
+
+
+// soluiton 3 with new Set()
+// Time O(n), space O(n)
+const solution = () => {
+    let hasSet = new Set()
+    for (let num of numbers) {
+        if (hasSet.has(num)) {
+            return true
+        }
+        hasSet.add(num)
+    }
+    return false
+}
+console.log(solution())
+
+
+// solution 4 with []
+// Time O(n), space O(n)
+let ary = []
+const solution2 = () => {
+    for (num of numbers) {
+        if(ary.includes(num)){
+           return true
+        }
+        ary.push(num)
+    }
+    return false
+}
+
+solution2()
+
+// solution 5 with Set
+const numberSet=[...new Set(numbers)]
+function solution3(){
+    return numberSet!==numbers
+}
+console.log(solution3())
